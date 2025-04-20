8d891ab6c307eb4f89460b6b37ed488de1cba826
Daria Eliseeva <elisey422das@gmail.com>
2025-04-20 13:13:26 +0200

    try repeat function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
-fun main() {
-    println("Введите свой возраст:")
-    val ageInput = readln()
-    val age = ageInput.toIntOrNull()
-    if (age != null) {
-        println("через год вам будет ${age + 1}")
-    } else {
-        println("Пожалуйста, введите корректное число.")
+//fun main() {
+//    println("Введите свой возраст:")
+//    val ageInput = readln()
+//    val age = ageInput.toIntOrNull()
+//    if (age != null) {
+//        println("через год вам будет ${age + 1}")
+//    } else {
+//        println("Пожалуйста, введите корректное число.")
+//    }
+//}
+fun main () {
+    println("Введите свое имя:")
+    val name = readln()
+    repeat(5){
+        println("Hello $name")
     }
 }
